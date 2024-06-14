2ba1b82697d5899b1e2e0ba129a50c6f5de3a061
Naasor <amofasandra7@gmail.com>
2024-06-14 17:54:12 +0000

    Markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -5,94 +5,121 @@ Usage: ./markdown2html.py input_file.md output_file.html
 """
 
 
+import os
 import sys
-import os.path
 import re
 import hashlib
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        exit(1)
+if len(sys.argv) == 1:
+    sys.exit(1)
 
-    if not os.path.isfile(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        exit(1)
+if not os.path.isfile(sys.argv[1]):
+    sys.exit(2)
 
-    with open(sys.argv[1]) as read:
-        with open(sys.argv[2], 'w') as html:
-            unordered_start, ordered_start, paragraph = False, False, False
-            # bold syntax
-            for line in read:
-                line = line.replace('**', '<b>', 1)
-                line = line.replace('**', '</b>', 1)
-                line = line.replace('__', '<em>', 1)
-                line = line.replace('__', '</em>', 1)
+ifile = sys.argv[1]
+ofile = re.sub('\.(md|markdown)$', '', ifile) + '.html'
 
-                # md5
-                md5 = re.findall(r'\[\[.+?\]\]', line)
-                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
-                if md5:
-                    line = line.replace(md5[0], hashlib.md5(
-                        md5_inside[0].encode()).hexdigest())
+with open(ifile, 'r') as ifile:
+    ifile_str = ifile.read() + '\n'
 
-                # remove the letter C
-                remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
-                remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
-                if remove_letter_c:
-                    remove_c_more = ''.join(
-                        c for c in remove_c_more[0] if c not in 'Cc')
-                    line = line.replace(remove_letter_c[0], remove_c_more)
+with open(ofile, 'w') as ofile:
+    B = False  # Bold
+    I = False  # Italic
+    S = False  # Strikethrough
+    c = False  # Inline code
+    C = False  # Multiline code
+    Q = 0
+    p = False  # Paragraph
+    i = 0
 
-                length = len(line)
-                headings = line.lstrip('#')
-                heading_num = length - len(headings)
-                unordered = line.lstrip('-')
-                unordered_num = length - len(unordered)
-                ordered = line.lstrip('*')
-                ordered_num = length - len(ordered)
-                # headings, lists
-                if 1 <= heading_num <= 6:
-                    line = '<h{}>'.format(
-                        heading_num) + headings.strip() + '</h{}>\n'.format(
-                        heading_num)
+    while i < len(ifile_str) - 2:
+        ch = ifile_str[i]
+        i += 1
 
-                if unordered_num:
-                    if not unordered_start:
-                        html.write('<ul>\n')
-                        unordered_start = True
-                    line = '<li>' + unordered.strip() + '</li>\n'
-                if unordered_start and not unordered_num:
-                    html.write('</ul>\n')
-                    unordered_start = False
+        if ch != '\n' and not p:
+            ofile.write('<p>')
+            p = True
 
-                if ordered_num:
-                    if not ordered_start:
-                        html.write('<ol>\n')
-                        ordered_start = True
-                    line = '<li>' + ordered.strip() + '</li>\n'
-                if ordered_start and not ordered_num:
-                    html.write('</ol>\n')
-                    ordered_start = False
+        if ch in ('*', '_'):
+            if C:
+                ofile.write(ch)
+                continue
+            if ifile_str[i] in ('*', '_'):
+                ofile.write(f'<{"/"*B}b>')
+                B = not B
+                i += 1
+            else:
+                ofile.write(f'<{"/"*I}i>')
+                I = not I
 
-                if not (heading_num or unordered_start or ordered_start):
-                    if not paragraph and length > 1:
-                        html.write('<p>\n')
-                        paragraph = True
-                    elif length > 1:
-                        html.write('<br/>\n')
-                    elif paragraph:
-                        html.write('</p>\n')
-                        paragraph = False
+        elif ch == '`':
+            ch_b, ch_c = ifile_str[i], ifile_str[i + 1]
+            if ch == ch_b == ch_c:
+                ofile.write(f'<{"/"*C}code>')
+                C = not C
+                i += 2
+            else:
+                if C:
+                    ofile.write(ch)
+                    continue
+                ofile.write(f'<{"/"*c}code>')
+                c = not c
 
-                if length > 1:
-                    html.write(line)
+        elif ch == '~':
+            if C:
+                ofile.write(ch)
+                continue
+            if ifile_str[i] == '~':
+                ofile.write(f'<{"/"*S}del>')
+                S = not S
+                i += 1
 
-            if unordered_start:
-                html.write('</ul>\n')
-            if ordered_start:
-                html.write('</ol>\n')
-            if paragraph:
-                html.write('</p>\n')
-    exit (0)
+        elif ch == '[':
+            if re.match('^\[.*\]\(.*(".*"|)\)$', ifile_str[i - 1:].split(')', 1)[0] + ')'):
+                name = ''
+                link = ''
+                alt = ''
+                s = ifile_str[i:].split(')', 1)[0] + ')'
+                i += len(s)
+                name = s.split(']')[0]
+                link = s.split('(')[1].split(')')[0]
+                if '"' in link:
+                    alt = link.split('"')[1].split('"')[0]
+                    link = link.split('"')[0].strip()
+                ofile.write(f'<a href="{link}" title="{alt}">{name}</a>')
+
+        elif ch == '\n':
+            if C:
+                ofile.write(ch)
+                continue
+
+            if c:
+                ofile.write(f'</code>')
+
+            if not p:
+                ofile.write('<br>')
+            elif ifile_str[i] == '\n':
+                ofile.write('</p>\n')
+                p = False
+                i += 1
+
+                if B:
+                    ofile.write(f'</b>')
+                    B = False
+                if I:
+                    ofile.write(f'</i>')
+                    I = False
+                if S:
+                    ofile.write(f'</del>')
+                    S = False
+            elif not ifile_str[i]:
+                if p:
+                    ofile.write('</p>\n')
+                else:
+                    ofile.write('\n')
+            else:
+                ofile.write('<br>')
+            ofile.write('\n')
+        else:
+            ofile.write(ch)
+
